5accc5586a76a3b9b02ee6a68c196321f59a8125
Alejandro <ahcorde@olivo.(none)>
2014-09-10 15:36:56 +0200

    GoPro Streamimg

---
+++ ros/beginner_camera/src/gopro.cpp
@@ -0,0 +1,47 @@
+#include "ros/ros.h"
+
+#include <sensor_msgs/fill_image.h>
+#include <self_test/self_test.h>
+#include <image_transport/image_transport.h>
+
+#include <sstream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "image_raw");
+
+    ros::NodeHandle n;
+
+    image_transport::Publisher image_pub;
+
+    image_transport::ImageTransport it(n);
+    image_pub = it.advertise("image_raw", 1);
+
+    sensor_msgs::Image img_;
+
+    ros::Rate loop_rate(10);
+
+    cv::Mat frame;
+    cv::VideoCapture cap("http://10.5.5.9:8080/live/amba.m3u8");
+
+    int count = 0;
+    while (ros::ok()){
+        cap>> frame;
+
+        fillImage(img_, "bgr8", frame.rows, frame.cols, frame.channels() * frame.cols, frame.data);
+        cv::imshow("imagen", frame);
+        cv::waitKey(20);
+
+        image_pub.publish(img_);
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        ++count;
+    }
+    return 0;
+}
